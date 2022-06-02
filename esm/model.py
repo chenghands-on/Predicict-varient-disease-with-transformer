795dba7f2af1da5dc5f9d0d073e374456bfd68aa
Harry xie <20307130043@fudan.edu.cn>
2022-06-02 15:49:28 +0800

    change model.py

--- esm/model.py
+++ esm/model.py
@@ -389,6 +389,14 @@ class MSATransformer(nn.Module):
         if (layer_idx + 1) in repr_layers:
             hidden_representations[layer_idx + 1] = x
         x = self.lm_head(x)
+        # 我新做的调试
+        from torch import nn
+        layer_last=nn.Linear(self.alphabet_size,self.alphabet_size)
+        def init_weights(m):
+            if type(m) == nn.Linear:
+                nn.init.xavier_uniform_(m.weight)
+        layer_last.apply(init_weights);
+        x=layer_last(x)
 
         result = {"logits": x, "representations": hidden_representations}
         if need_head_weights:
